906a891266741e6c728a3688a59cea85a617187b
Ihor Shchypakov <ishchypakov@doz.pl>
2024-07-29 12:37:12 +0200

    init

--- app/api/end/route.ts
+++ app/api/end/route.ts
@@ -17,11 +17,10 @@ export async function POST(req: NextRequest): Promise<Response> {
     }
     const headers = new Headers();
     headers.set("Location", `${process.env.NEXT_PUBLIC_BASE_URL}/`);
-    const response = NextResponse.redirect(
+    return NextResponse.redirect(
         `${process.env.NEXT_PUBLIC_BASE_URL}/${path}`,
         302,
     );
-    return response;
 }
 
 export const dynamic = "force-dynamic";
