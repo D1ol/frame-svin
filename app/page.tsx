56ce1c34f1a99e69a703428a7ac7ed8ea3083156
Ihor Shchypakov <ishchypakov@doz.pl>
2024-07-29 13:57:30 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 import type { Metadata } from 'next';
 
 const frameMetadata = getFrameMetadata({
+  accepts: {},
   buttons: [
     {
       label: "Begin"
     }
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmYD28wBFnyCswXbogqznjd3KzrzGvkvfrHqfccTuBJBxh/0.png`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${process.env.NEXT_PUBLIC_CID}/0.png`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
@@ -18,7 +19,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Cosmic Cowboys',
     description: 'A frame telling the story of Cosmic Cowboys',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmYD28wBFnyCswXbogqznjd3KzrzGvkvfrHqfccTuBJBxh/0.png`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${process.env.NEXT_PUBLIC_CID}/0.png`],
   },
   other: {
     ...frameMetadata,
